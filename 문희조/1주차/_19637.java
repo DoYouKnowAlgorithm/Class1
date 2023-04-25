65875555bb27709aa589ea772305a17d17326ec6
Moon He Jow <gmlwh124@naver.com>
2023-04-26 01:18:12 +0900

    refactor: 19637

--- 문희조/1주차/_19637.java
+++ 문희조/1주차/_19637.java
@@ -1,86 +1,68 @@
 package Algorithm.BOJ;
 
 import java.io.*;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class _19637 {
     static BufferedReader br = getBufferedReader();
     static BufferedWriter bw = getBufferedWriter();
-    static HashMap<Integer, String> table = new HashMap<>();
-    static Integer[] keys;
+    static String[] titles;
+    static int[] values;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = getTokenizer();
-        int n = getTokenAsInt(st);
-        int m = getTokenAsInt(st);
+        int n = getIntFromToken(st);
+        int m = getIntFromToken(st);
 
-        initSetup(n);
+        initTable(n);
         solution(n, m);
     }
 
-    private static void initSetup(int n) throws IOException {
+    private static void initTable(int n) throws IOException {
+        titles = new String[n];
+        values = new int[n];
+
         for (int i = 0 ; i < n ; i++) {
             StringTokenizer st = getTokenizer();
-            String value = getToken(st);
-            Integer key = getTokenAsInt(st);
-
-            if (isNotDuplicated(key)) {
-                table.put(key, value);
-            }
+            titles[i] = getStringFromToken(st);;
+            values[i] = getIntFromToken(st);
         }
-
-        keys = table.keySet().toArray(new Integer[0]);
     }
 
     private static void solution(int n, int m) throws IOException {
         for (int i = 0; i < m; i++) {
-            int input = readInputAsInt();
-            String title = binarySearch(input);
-            print(title);
+            int input = getIntFromInput();
+            String title = binarySearch(0, n - 1, input);
+            bw.write(title + "\n");
         }
+        bw.flush();
     }
 
-    private static String binarySearch(int target) {
-        int start = 0;
-        int end = keys.length - 1;
+    private static String binarySearch(int start, int end, int target) {
         int middle;
 
         while (start <= end) {
             middle = (start + end) / 2;
-            if (target > keys[middle]) {
+            if (target > values[middle]) {
                 start = middle + 1;
             } else {
                 end = middle - 1;
             }
         }
 
-        return getTitle(keys[start]);
+        return titles[start];
     }
 
-    private static String getTitle(Integer key) {
-        return table.get(key);
-    }
-
-    private static boolean isNotDuplicated(Integer key) {
-        return !table.containsKey(key);
-    }
-
-    private static int readInputAsInt() throws IOException {
+    private static int getIntFromInput() throws IOException {
         return Integer.parseInt(br.readLine());
     }
 
-    private static int getTokenAsInt(StringTokenizer st) {
+    private static int getIntFromToken(StringTokenizer st) {
         return Integer.parseInt(st.nextToken());
     }
 
-    private static String getToken(StringTokenizer st) {
+    private static String getStringFromToken(StringTokenizer st) {
         return st.nextToken();
-    }
-
-    private static void print(String title) throws IOException {
-        bw.write(title + "\n");
-        bw.flush();
     }
 
     private static StringTokenizer getTokenizer() throws IOException {
